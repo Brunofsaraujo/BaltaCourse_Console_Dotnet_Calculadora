e4c7e759f7fe393c4171d9c571a45017a7d263dd
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-01-09 17:09:51 -0300

    Calculadora: Criação projeto

---
+++ BaltaCourse_Console_Dotnet_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaltaCourse_Console_Dotnet_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+        }
+    }
+}
